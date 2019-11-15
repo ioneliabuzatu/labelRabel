de6392c673da1ae999cf63651bf2c93f07ccb869
buzatuionelia <buzatuionelia@gmail.com>
2019-11-15 15:03:19 +0100

    main file

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// https://github.com/tomrunia/Image-Annotation-Tool/blob/master/ImageAnnotationTool/ImageAnnotator.cpp
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <opencv2/opencv.hpp>
+// #include <opencv2/core/version.hpp>
+// #include <opencv2/imgproc/imgproc.hpp>
+// #include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/optflow.hpp>
+// #include <opencv2/video/tracking.hpp>
+// #include <string>
+
+int main() {
+    int hello = 3;
+
+}
